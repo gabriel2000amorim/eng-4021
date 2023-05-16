f3674863dacf8a8bf8fda768cc0eb982346c7ea1
gabriel2000amorim <132674467+gabriel2000amorim@users.noreply.github.com>
2023-05-15 22:41:57 -0300

    Update models.py

--- gabapp/models.py
+++ gabapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+# Definição do modelo de dados para uma tarefa
 class Task(models.Model):
-  title = models.CharField(max_length = 50)
-  description = models.TextField()
-  due_date = models.DateField()
-  done = models.BooleanField()
+    # Campo de caractere para o título da tarefa
+    title = models.CharField(max_length=50)
+    # Campo de texto para a descrição da tarefa
+    description = models.TextField()
+    # Campo de data para a data de vencimento da tarefa
+    due_date = models.DateField()
+    # Campo booleano para indicar se a tarefa foi concluída ou não
+    done = models.BooleanField()
